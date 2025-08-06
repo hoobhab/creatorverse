1afde3ced1e95f8a0a3d6fa590de4546d03e3b96
hoobhab <hoobhab3@gmail.com>
2025-08-06 13:51:55 -0700

    Implemented functionality for the index page. All creators should now be displayed on the index.

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { useState } from "react";
+import { React } from "react";
+import { useState, useEffect } from "react";
+import Creator from '../components/Creator';
+import { supabase } from '../client';
 
-const ShowCreators = (props) => {
+const ShowCreators = () => {
+
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    const fetchCreators = async () => {
+      const { data } = await supabase
+        .from('creators')
+        .select().order("created_at", {ascending: false});
+        setCreators(data)
+    }
+    fetchCreators()
+  }, [])
+
   return (
-    <div className="Creator">
-                Placeholder
-
+    <div className="show-creators">
+      {
+        creators && creators.length > 0 ?
+          creators.map((creator, index) => 
+            <Creator
+            name = {creator.name}
+            url = {creator.url}
+            description = {creator.description}
+            imageURL = {creator.imageURL}
+            />
+          ) : <p>Nothing here for now...</p>
+      }
     </div>
   );
 };
